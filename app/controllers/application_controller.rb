531845426c08e04c26ee9309a8230e87cb3413a3
yossu-y <yangyuub@gmail.com>
2022-04-12 18:08:56 +0000

    [finish]6章

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configue_parmitted_permeters, if: :devise_vontroller?
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer .permit(:sign_up, keys:[:name])
+  end
 end

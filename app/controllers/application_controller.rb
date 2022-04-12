574912b5a09dc5cbe1c8a23ba96844d6a332f256
yossu-y <yangyuub@gmail.com>
2022-04-12 18:47:16 +0000

    [finish]6章（更新）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :configue_parmitted_permeters, if: :devise_vontroller?
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
-  
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer .permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
 end

aa86ef5b92470b4a8f94f101191b329854b169cc
yossu-y <yangyuub@gmail.com>
2022-04-13 10:48:34 +0000

    [finish]14章

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :post_images, only: [:new, :create, :index, :show,:destroy]
+  resources :users, only: [:show, :edit]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 

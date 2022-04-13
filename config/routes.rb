e06c8a0d30da0f9e9faa1a8255b75dd8514fa391
yossu-y <yangyuub@gmail.com>
2022-04-13 04:28:33 +0000

    [finish]9章

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+   devise_for :users
   root to: "homes#top"
+  
+  resources :post_images, only: [:new, :index, :show]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/homes/about",as: 'about'
   

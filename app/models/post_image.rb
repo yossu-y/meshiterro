f8c2f80a00dfd05d2239dd0f850aff6625103257
yossu-y <yangyuub@gmail.com>
2022-04-14 06:34:16 +0000

    [finish]20章

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,8 @@ class PostImage < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def get_image
     unless image.attached?
@@ -10,6 +12,10 @@ class PostImage < ApplicationRecord
     else
       image
     end
+  end
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root to: "homes#top"
   get "/homes/about",as: 'about'
-  devise_for :users
-
-  resources :post_images, only: [:new, :create, :index, :show,:destroy]
-  resources :users, only: [:show, :edit, :update]
-
+  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy] end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-
+  resources :users, only: [:show, :edit, :update]
 end
